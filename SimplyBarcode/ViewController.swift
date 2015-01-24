cd094c51f9439565d84bb8b091f3401527dba62b
Shrikar Archak <shrikar@viralheat.com>
2015-01-24 15:44:41 -0800

    Initial Commit

---
+++ SimplyBarcode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimplyBarcode
+//
+//  Created by Shrikar Archak on 1/24/15.
+//  Copyright (c) 2015 Shrikar Archak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
